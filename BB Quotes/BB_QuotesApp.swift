84c95455b11aa05097303f11c00b4f76f132116b
Justin Maronde <myemailisjustin@me.com>
2024-03-20 10:40:58 -0400

    Initial Commit

---
+++ BB Quotes/BB_QuotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  BB_QuotesApp.swift
+//  BB Quotes
+//
+//  Created by Justin Maronde on 3/20/24.
+//
+
+import SwiftUI
+
+@main
+struct BB_QuotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
