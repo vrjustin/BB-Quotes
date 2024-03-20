d70d251885840c669af6450c77e953d56d3b8d44
Justin Maronde <myemailisjustin@me.com>
2024-03-20 14:18:42 -0400

    finish quoteview styling using hardcoded data; quoteview from tabview

--- BB Quotes/Views/QuoteView.swift
+++ BB Quotes/Views/QuoteView.swift
@@ -16,7 +16,10 @@ struct QuoteView: View {
                     .frame(width: geoReader.size.width * 2.7, height: geoReader.size.height * 1.2)
                 
                 VStack {
+                    Spacer(minLength: 140)
+                    
                     Text("\"You either run from things or you face them, Mr. White.\"")
+                        .minimumScaleFactor(0.5)
                         .multilineTextAlignment(.center)
                         .foregroundColor(.white)
                         .padding()
@@ -37,6 +40,22 @@ struct QuoteView: View {
                     })
                     .frame(width: geoReader.size.width / 1.1, height: geoReader.size.height / 1.8)
                     .cornerRadius(80)
+                    
+                    Spacer()
+                    
+                    Button {
+                        
+                    } label: {
+                        Text("Get Random Quote")
+                            .font(.title)
+                            .foregroundColor(.white)
+                            .padding()
+                            .background(.breakingBadGreen)
+                            .cornerRadius(7)
+                            .shadow(color: .breakingBadYellow, radius: 2)
+                    }
+                    
+                    Spacer(minLength: 180)
                 }
                 .frame(width: geoReader.size.width)
             }
