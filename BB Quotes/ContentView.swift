8550a53f6ce2b3670fd47639f37abefd83f1f005
Justin Maronde <myemailisjustin@me.com>
2024-03-20 10:52:30 -0400

    add in our tabbar, initial items, and basic styling

--- BB Quotes/ContentView.swift
+++ BB Quotes/ContentView.swift
@@ -9,13 +9,20 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        TabView {
+            Text("Breaking Bad View")
+                .tabItem {
+                    Label("Breaking Bad", systemImage: "tortoise")
+                }
+            Text("Better Call Saul View")
+                .tabItem {
+                    Label("Better Call Saul", systemImage: "briefcase")
+                }
         }
-        .padding()
+        .onAppear {
+            UITabBar.appearance().scrollEdgeAppearance = UITabBarAppearance()
+        }
+        .preferredColorScheme(.dark)
     }
 }
 
