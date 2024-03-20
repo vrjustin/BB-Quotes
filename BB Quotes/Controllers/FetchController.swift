45897beee0c5c7a497956d87ba5120be92ff0392
Justin Maronde <myemailisjustin@me.com>
2024-03-20 12:27:37 -0400

    build out our FetchController and initial fetchQuote networking

--- BB Quotes/Controllers/FetchController.swift
+++ BB Quotes/Controllers/FetchController.swift
@@ -6,3 +6,33 @@
 //
 
 import Foundation
+
+struct FetchController {
+    enum NetworkError: Error {
+        case badURL, badResponse
+    }
+    
+    // https://breaking-bad-api-six.vercel.app/api/quotes/random?production=Breaking+Bad
+    private let baseURL = URL(string: "https://breaking-bad-api-six.vercel.app/api")!
+    
+    func fetchQuote(from show: String) async throws -> Quote {
+        let quoteURL = baseURL.appending(path: "quotes/random")
+        var quoteComponents = URLComponents(url: quoteURL, resolvingAgainstBaseURL: true)
+        let quoteQueryItem = URLQueryItem(name: "production", value: show.replacingOccurrences(of: " ", with: "+"))
+        quoteComponents?.queryItems = [quoteQueryItem]
+        
+        guard let fetchURL = quoteComponents?.url else {
+            throw NetworkError.badURL
+        }
+        
+        let (data, response)  = try await URLSession.shared.data(from: fetchURL)
+        
+        guard let response = response as? HTTPURLResponse, response.statusCode == 200 else {
+            throw NetworkError.badResponse
+        }
+        
+        let quote = try JSONDecoder().decode(Quote.self, from: data)
+        
+        return quote
+    }
+}
