b3f8f6d431420a15ab3e928382ae5397e860d005
Justin Maronde <myemailisjustin@me.com>
2024-03-21 07:16:12 -0400

    use the viewmodel to dispay on quoteview

--- BB Quotes/Views/QuoteView.swift
+++ BB Quotes/Views/QuoteView.swift
@@ -8,43 +8,64 @@
 import SwiftUI
 
 struct QuoteView: View {
+    @StateObject private var viewModel = ViewModel(controller: FetchController())
+    
+    let show: String
+    
     var body: some View {
         GeometryReader { geoReader in
             ZStack {
-                Image("breakingbad")
+                Image(show.lowercased().filter { $0 != " "})
                     .resizable()
                     .frame(width: geoReader.size.width * 2.7, height: geoReader.size.height * 1.2)
                 
                 VStack {
                     Spacer(minLength: 140)
                     
-                    Text("\"You either run from things or you face them, Mr. White.\"")
-                        .minimumScaleFactor(0.5)
-                        .multilineTextAlignment(.center)
-                        .foregroundColor(.white)
-                        .padding()
-                        .background(.black.opacity(0.5))
-                        .cornerRadius(25)
-                        .padding(.horizontal)
-                    
-                    ZStack(alignment: .bottom, content: {
-                        Image("jessepinkman")
-                            .resizable()
-                            .scaledToFill()
-                        
-                        Text("Jesse Pinkman")
+                    switch viewModel.status {
+                    case .notStarted:
+                        EmptyView()
+                    case .fetching:
+                        ProgressView()
+                    case .success(let data):
+                        Text("\"\(data.quote.quote)\"")
+                            .minimumScaleFactor(0.5)
+                            .multilineTextAlignment(.center)
                             .foregroundColor(.white)
-                            .padding(10)
-                            .frame(maxWidth: .infinity)
-                            .background(.ultraThinMaterial)
-                    })
-                    .frame(width: geoReader.size.width / 1.1, height: geoReader.size.height / 1.8)
-                    .cornerRadius(80)
+                            .padding()
+                            .background(.black.opacity(0.5))
+                            .cornerRadius(25)
+                            .padding(.horizontal)
+                        
+                        ZStack(alignment: .bottom, content: {
+                            AsyncImage(url: data.character.images[0]) { image in
+                                    image
+                                    .resizable()
+                                    .scaledToFill()
+                            } placeholder: {
+                                ProgressView()
+                            }
+                            .frame(width: geoReader.size.width / 1.1, height: geoReader.size.height / 1.8)
+                            
+                            Text(data.quote.character)
+                                .foregroundColor(.white)
+                                .padding(10)
+                                .frame(maxWidth: .infinity)
+                                .background(.ultraThinMaterial)
+                        })
+                        .frame(width: geoReader.size.width / 1.1, height: geoReader.size.height / 1.8)
+                        .cornerRadius(80)
+                    case .failed(let error):
+                        EmptyView()
+                    }
                     
                     Spacer()
                     
                     Button {
-                        
+                        //Action of button press here
+                        Task {
+                            await viewModel.getData(for:show)
+                        }
                     } label: {
                         Text("Get Random Quote")
                             .font(.title)
@@ -66,6 +87,6 @@ struct QuoteView: View {
 }
 
 #Preview {
-    QuoteView()
-//        .preferredColorScheme(.dark)
+    QuoteView(show: "Breaking Bad")
+        .preferredColorScheme(.dark)
 }
